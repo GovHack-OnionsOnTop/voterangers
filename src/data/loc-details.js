89613f48361396652eaab83265ce42ed001527ed
Mahesh Samaradiwakara <mahesh.samaradiwakara@anz.com>
2020-08-15 16:38:08 +1000

    WIP-cleaned

---
+++ src/data/loc-details.js
@@ -0,0 +1,27 @@
+
+const locDetails = {
+   "result": [
+      {
+         "id":"40b5a523dfea62e9d4c711061398a7a3",
+         "name":"103 Fisher St, NY",
+         "lat":-29.9505,
+         "lng":124.0605,
+         "type":"stop",
+         "count":1,
+         "users_count":0,
+         "routes_count":2
+      }
+   ],
+   "info":{
+      "page":1,
+      "page_size":10,
+      "total_count":211,
+      "total_pages":22
+   },
+   "message":"Success",
+   "response":{
+      "code":200
+   }
+};
+
+export default locDetails;
